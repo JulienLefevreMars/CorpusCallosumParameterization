aa983cc0fa032212ee0ee98cd78e1432a39469a1
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2022-04-07 11:48:04 +0200

    Add Voxel spectral anamysis + vizu

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import nibabel as nb
+import numpy as np
+import voxel_spectral_analysis as vsa
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import networkx as nx
+import vizu as vz
 
 if __name__ =="__main__":
 	data_folder = "/home/julienlefevre/ownCloud/Documents/Recherche/Data/CorpusCallosum/isthme_du_corps_calleux/"
-	subject_name = "corpus_callosum_mask_26c_201"
+	subject_name = "corpus_callosum_mask_26c_256"
 	filename = data_folder + subject_name + ".nii.gz"
-	g = nb.load(data_folder + subject)
-	print(g)
+	g = nb.load(filename)
+	mask = np.asanyarray(g.dataobj)
+	(x,y,z) = vsa.image_to_points(mask,g.header['pixdim'][1:4])
+	
+	# 1. Visualize the points of the mask
+	vz.visualize_mask(g)
+
+	# 2. Compute Fiedler vector	
+	graph = vsa.image_to_graph(mask)
+	fiedler_vector = nx.fiedler_vector(graph)
+
+
