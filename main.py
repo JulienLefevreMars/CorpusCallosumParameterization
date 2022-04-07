46302464ac262ee3884c7e7b59db4452cc1f5e5c
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2022-04-07 12:14:17 +0200

    Vizu with equal axis

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import networkx as nx
 import vizu as vz
+import sys
 
 if __name__ =="__main__":
+	if len(sys.argv)==1:
+		name = "238"
+	else:
+		name = sys.argv[1]
 	data_folder = "/home/julienlefevre/ownCloud/Documents/Recherche/Data/CorpusCallosum/isthme_du_corps_calleux/"
-	subject_name = "corpus_callosum_mask_26c_256"
-	filename = data_folder + subject_name + ".nii.gz"
+	subject_name = "corpus_callosum_mask_26c_"
+	filename = data_folder + subject_name + name +  ".nii.gz"
 	g = nb.load(filename)
 	mask = np.asanyarray(g.dataobj)
 	(x,y,z) = vsa.image_to_points(mask,g.header['pixdim'][1:4])
@@ -21,4 +26,5 @@ if __name__ =="__main__":
 	graph = vsa.image_to_graph(mask)
 	fiedler_vector = nx.fiedler_vector(graph)
 
+	vz.visualize_fiedler(graph,fiedler_vector)
 
